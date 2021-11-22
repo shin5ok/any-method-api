e287a650190ee3c73f543a389200c774592a6526
Shingo <kawanos@google.com>
2021-11-23 00:55:03 +0900

    add path patterns for each method

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var paths = []string{"/", "/:p"}
+var paths = []string{"/", "/:p", "/:p/:v"}
 var port = os.Getenv("PORT")
 
 func CreateRoute() *gin.Engine {
