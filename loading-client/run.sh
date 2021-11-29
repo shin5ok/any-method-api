d4a4c9fba28f33167aa9fb28717a9ebd195ac311
Shingo <kawanos@google.com>
2021-11-29 13:21:05 +0900

    specify wait seconds

--- loading-client/run.sh
+++ loading-client/run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-
+n=0
+waitseconds=${1:-1}
+echo $seconds
 while :
 do
+	n=$(expr $n + 1)
+	printf "[%06d] Trying to load for $IP" $n
 	hey -c 2 -n 200 http://$IP
-	sleep 1
+	sleep $waitseconds
 done
