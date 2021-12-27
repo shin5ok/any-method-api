592f0b829b43ba1002e892523b0c2be39f7c2b75
Shingo <kawanos@google.com>
2021-12-27 21:43:40 +0900

    modify to make it work well

--- add-iam-policy.sh
+++ add-iam-policy.sh
@@ -1,2 +1,2 @@
 
-gcloud projects add-iam-policy-binding --member=serviceAccount:$(gcloud projects describe --format=json $PROJECT | jq .projectNumber -r)-compute@developer.gserviceaccount.com --role=roles/editor shingo-ar-slo1226
+gcloud projects add-iam-policy-binding --member=serviceAccount:$(gcloud projects describe --format=json $PROJECT | jq .projectNumber -r)-compute@developer.gserviceaccount.com --role=roles/editor $PROJECT
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 date=$(date '+%Y%m%d%H%M')
 TAG=$date
 echo "Building with TAG=$TAG"
-gcloud builds submit --pack=image=gcr.io/$PROJECT/any-method-api:$TAG --project=$PROJECT
+gcloud builds submit --pack=image=gcr.io/$PROJECT/${APPNAME}:$TAG --project=$PROJECT
