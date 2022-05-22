fc724e9a87a3c946296282c2d8b05c99dd757b3c
shin5ok <shin5ok@55mp.com>
2022-05-23 00:41:16 +0900

    get an argument for APPNAME

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 date=$(date '+%Y%m%d%H%M')
 TAG=$date
-APPNAME=any-method-api
+APPNAME=${1:-any-method-api}
 echo "Building with TAG=$TAG"
 gcloud builds submit --pack=image=gcr.io/$PROJECT/${APPNAME}:$TAG --project=$PROJECT
