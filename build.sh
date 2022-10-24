cf8c757553ab1c778f4e82b1882476c93bb680ad
shin5ok <shin5ok@55mp.com>
2022-10-24 12:03:33 +0900

    no use buildpacks to build faster

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ date=$(date '+%Y%m%d%H%M')
 TAG=${TAG:-${date}}
 APPNAME=${1:-any-method-api}
 echo "Building with TAG=$TAG"
-gcloud builds submit --pack=image=gcr.io/$PROJECT/${APPNAME}:$TAG --project=$PROJECT
+gcloud builds submit -t gcr.io/$PROJECT/${APPNAME}:$TAG --project=$PROJECT
