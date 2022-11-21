a57ae5c611b52e1a5741ad700a40dc2f60cf10f5
shin5ok <shin5ok@55mp.com>
2022-11-21 14:19:37 +0900

    fix it

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 date=$(date '+%Y%m%d%H%M')
 TAG=${TAG:-${date}}
-APPNAME=${1:-any-method-api}
 echo "Building with TAG=$TAG"
 gcloud builds submit -t gcr.io/$PROJECT/${APPNAME}:$TAG --project=$PROJECT
